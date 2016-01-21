07c39a54dce6721361278b80ca1a107fdc5754cd
Steven Buxton <breathesteve@gmail.com>
2016-01-21 11:22:13 -0600

    cookies

--- index.php
+++ index.php
@@ -1,28 +1,8 @@
 <?php
 
-	session_start();
+	//setcookie('id', '1234', time() + 60 * 60 *24);
 
-	$link = mysqli_connect('localhost', 'cl40-example-h7h', 'bkDFkY9^w', 'cl40-example-h7h');
+	setcookie('id', '', time() - 3600);
 
-	if (mysqli_connect_error()) {
-	 	 die("Could not connect to database");
-	}	 	
-
-	//$query = "INSERT INTO `users` (`name`, `email`, `password`) VALUES('John', 'john@example.com', 'test')";
-	 	
-	//$query = "UPDATE `users` SET `name =` 'John Smith' WHERE name = 'john'";
-	
-	//mysqli_query($link, $query);
-
-	$query = "SELECT * FROM users";
-
-	if ($result = mysqli_query($link, $query)) {
-		 	 while ($row = mysqli_fetch_array($result)) {
-		 	 print_r($row); 
-		 	}
-	} else {
-		 echo "It failed";
-	}
-
-	//session_destroy();
+	echo $_COOKIE['id'];
 ?>
