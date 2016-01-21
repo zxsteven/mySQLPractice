a4bd92ac0ef0524646c351434a41a5d12df8fcb1
Steven Buxton <breathesteve@gmail.com>
2016-01-21 11:17:54 -0600

    update query, session start, and session destroy

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+	session_start();
+
 	$link = mysqli_connect('localhost', 'cl40-example-h7h', 'bkDFkY9^w', 'cl40-example-h7h');
 
 	if (mysqli_connect_error()) {
@@ -8,11 +10,19 @@
 
 	//$query = "INSERT INTO `users` (`name`, `email`, `password`) VALUES('John', 'john@example.com', 'test')";
 	 	
-	 	$query = "UPDATE `users` SET `email`= 'email@hptmail.com' WHERE `name`= 'Jeff'";
+	//$query = "UPDATE `users` SET `name =` 'John Smith' WHERE name = 'john'";
+	
+	//mysqli_query($link, $query);
+
+	$query = "SELECT * FROM users";
+
 	if ($result = mysqli_query($link, $query)) {
-		 	 $row = mysqli_fetch_array($result);
+		 	 while ($row = mysqli_fetch_array($result)) {
 		 	 print_r($row); 
+		 	}
 	} else {
 		 echo "It failed";
 	}
+
+	//session_destroy();
 ?>
