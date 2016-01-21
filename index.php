0f10e8b82f968b4d4f03f5e6a0d0f1f0a5f07806
Steven Buxton <breathesteve@gmail.com>
2016-01-20 22:25:51 -0600

    retrieve data

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
 
-	mysqli_connect('localhost', 'cl40-example-h7h', 'bkDFkY9^w', 'cl40-example-h7h');
+	$link = mysqli_connect('localhost', 'cl40-example-h7h', 'bkDFkY9^w', 'cl40-example-h7h');
 
 	if (mysql_connect_error()) {
-		echo "Could not connect.";
+		die("Could not connect.");
 	}
+
+	$query = 'SELECT * FROM users';
+
+	if ($result = mysqli_query($link, $query)) {
+		$row = mysql_fetch_array($result);
+		print_r($row);
+	} else {
+		echo "Failed";
+	}
+
+
 ?>
